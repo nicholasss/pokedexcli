21bfb242fcc599dce6e7364a9e3ed7c12d1e93d9
Nicholas <nicholaszingleman@gmail.com>
2025-01-22 12:45:13 -0500

    added two commands and a valid command map

--- main.go
+++ main.go
@@ -7,10 +7,51 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var validCommands map[string]cliCommand
+
+func init() {
+
+	validCommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
+}
+
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	words := strings.Fields(text)
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, ci := range validCommands {
+		fmt.Printf("%s: %s\n", ci.name, ci.description)
+	}
+
+	return nil
 }
 
 func main() {
@@ -29,6 +70,13 @@ func main() {
 
 		args := cleanInput(scanner.Text())
 		command := args[0]
-		fmt.Println("Your command was:", command)
+
+		validCommand, exists := validCommands[command]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		validCommand.callback()
 	}
 }
