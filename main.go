44c3bffb369cc6034990f2d9054d79eaea3d63eb
Nicholas <nicholaszingleman@gmail.com>
2025-01-22 11:43:32 -0500

    basic repl working, added log file to gitignore

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
+	text = strings.ToLower(text)
 	words := strings.Fields(text)
 	return words
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ") // prompt
+
+		if ok := scanner.Scan(); !ok {
+			continue // no text provided
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Error occured: %s\n", err)
+		}
+
+		args := cleanInput(scanner.Text())
+		command := args[0]
+		fmt.Println("Your command was:", command)
+	}
 }
